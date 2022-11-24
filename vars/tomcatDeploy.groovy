bb56c834a014460133def25037e0b97c2b99846e
ganeshravilla205 <116986844+ganeshravilla205@users.noreply.github.com>
2022-11-24 19:50:56 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,3 +1,8 @@
-def call(){
-  echo "tomcat deploying to shared library"
+def call(credens, userName, ipName, jarName){
+  sshagent([credens]) {
+          //copy jar file to tomcat folder
+          sh "scp -o StrictHostKeyChecking=no /target/${jarName}.jar ${userName}@${ipName}:/opt/tomcat9/webapps"
+          sh "ssh ${userName}@${ipName} /opt/tomcat9/bin/shutdown.sh"
+          sh "ssh ${userName}@${ipName} /opt/tomcat9/bin/startup.sh"
+        }
 }
